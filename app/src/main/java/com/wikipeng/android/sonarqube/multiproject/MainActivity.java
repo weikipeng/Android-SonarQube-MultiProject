777f57ff4edc5e9e5379d359cf3f1481e1ce9825
wiki <wiki.peng@qq.com>
2015-04-23 12:11:47 +0800

    init android sonarqube multi project with right config

--- app/src/main/java/com/wikipeng/android/sonarqube/multiproject/MainActivity.java
+++ app/src/main/java/com/wikipeng/android/sonarqube/multiproject/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.weiauto.develop.tool.FOpenLog;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FOpenLog.e("android sonarqube multi project example");
     }
 
 
